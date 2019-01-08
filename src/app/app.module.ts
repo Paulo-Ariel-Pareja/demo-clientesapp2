656ced75efb29660f64dfe50515aef3298eb3276
Paulo Pareja <paulo.pareja@hotmail.es>
2019-01-08 14:00:58 -0300

    optimizacion de codigo. se agrego interceptores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormComponent } from './clientes/form.component';
 import { FormsModule } from '@angular/forms';
 import { PaginatorComponent } from './paginator/paginator.component';
@@ -19,6 +19,8 @@ import { DetalleComponent } from './clientes/detalle/detalle.component';
 import { LoginComponent } from './usuarios/login.component';
 import { AuthGuard } from './usuarios/guards/auth.guard';
 import { RoleGuard } from './usuarios/guards/role.guard';
+import { TokenInterceptor } from './usuarios/interceptors/token.interceptor';
+import { AuthInterceptor } from './usuarios/interceptors/auth.interceptor';
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full' },
@@ -54,7 +56,11 @@ export class AppRoutingModule { }
     MatDatepickerModule, 
     MatMomentDateModule
   ],
-  providers: [ClienteService],
+  providers: [
+    ClienteService,
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-//import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente.js';
-import { of, Observable, throwError } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpRequest, HttpEvent } from '@angular/common/http';
-import { map, catchError, tap } from 'rxjs/operators';
-import swal from 'sweetalert2';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { formatDate, DatePipe } from '@angular/common';
 import { Region } from './region.js';
-import { AuthService } from '../usuarios/auth.service.js';
 
 @Injectable({
   providedIn: 'root'
@@ -16,44 +12,11 @@ import { AuthService } from '../usuarios/auth.service.js';
 export class ClienteService {
 
   private urlEndPoint: string = "http://localhost:8080/api/clientes";
-  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-  constructor(private http: HttpClient, private router: Router, private authService: AuthService) { }
-
-  private agregarAuthorizationHeader(){
-    let token = this.authService.token;
-    if(token != null){
-      return this.httpHeaders.append('Authorization', 'Bearer ' + token);
-    }
-    return this.httpHeaders;
-  }
-
-  private isNoAutorizado(e): boolean {
-    if (e.status == 401) {
-
-      if(this.authService.isAuthenticated){
-        this.authService.logout();
-      }
-
-      this.router.navigate(['/login']);
-      return true;
-    }
-
-    if (e.status == 403) {
-      swal('Acceso denegado', `${this.authService.usuario.username} no tienes acceso a esta pagina.`, 'warning');
-      this.router.navigate(['/clientes']);
-      return true;
-    }
-    return false;
-  }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getRegiones(): Observable<Region[]> {
-    return this.http.get<Region[]>(this.urlEndPoint + '/regiones', {headers: this.agregarAuthorizationHeader()}).pipe(
-      catchError(e => {
-        this.isNoAutorizado(e);
-        return throwError(e);
-      })
-    );
+    return this.http.get<Region[]>(this.urlEndPoint + '/regiones');
   }
 
   getClientes(page: number): Observable<any> {
@@ -70,61 +33,42 @@ export class ClienteService {
   }
 
   create(cliente: Cliente): Observable<Cliente> {
-    return this.http.post(this.urlEndPoint, cliente, { headers: this.agregarAuthorizationHeader() }).pipe(
+    return this.http.post(this.urlEndPoint, cliente).pipe(
       map((response: any) => response.cliente as Cliente),
       catchError(e => {
-        if (this.isNoAutorizado(e)) {
-          return throwError(e);
-        }
         if (e.status == 400) {
           return throwError(e);
         }
-
-        swal(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
       })
     );
   }
 
   getCliente(id): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`, { headers: this.agregarAuthorizationHeader() }).pipe(
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
       catchError(e => {
-        if (this.isNoAutorizado(e)) {
-          return throwError(e);
+        if (e != 401) {
+          this.router.navigate(['/clientes']);
         }
-        this.router.navigate(['/clientes']);
-        swal('Error al editar.', e.error.mensaje, 'error');
         return throwError(e);
       })
     );
   }
 
   update(cliente: Cliente): Observable<any> {
-    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, { headers: this.agregarAuthorizationHeader()  }).pipe(
+    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente).pipe(
       catchError(e => {
-
-        if (this.isNoAutorizado(e)) {
-          return throwError(e);
-        }
-
         if (e.status == 400) {
           return throwError(e);
         }
-
-        swal(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
       })
     );
   }
 
   delete(id: number): Observable<Cliente> {
-    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, { headers: this.agregarAuthorizationHeader()  }).pipe(
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
       catchError(e => {
-        if (this.isNoAutorizado(e)) {
-          return throwError(e);
-        }
-
-        swal(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
       })
     );
@@ -135,23 +79,11 @@ export class ClienteService {
     formData.append("archivo", archivo);
     formData.append("id", id);
 
-    let httpHeaders = new HttpHeaders();
-    let token = this.authService.token;
-    if(token != null){
-      httpHeaders = httpHeaders.append('Authorization', 'Bearer ' + token);
-    }
-
     const req = new HttpRequest('POST', `${this.urlEndPoint}/upload`, formData, {
-      reportProgress: true,
-      headers: httpHeaders
+      reportProgress: true
     });
 
-    return this.http.request(req).pipe(
-      catchError(e => {
-        this.isNoAutorizado(e);
-        return throwError(e);
-      })
-    );
+    return this.http.request(req);
   }
 }
 
